9cd29b688289e85608921b20fb5c04eaebcce8bf
joao holz <joaopaulo.paivaholz@gmail.com>
2020-12-15 23:42:12 -0300

    test

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from modules.from_csv_to_gs import send_csv
 from modules.from_db_to_csv import create_csv
 
 def main():
+    ## make the query and wright in the current directory
     create_csv()
+    ## send csv to google sheets
     send_csv()
-    
+
 if __name__ == "__main__":
     main()
