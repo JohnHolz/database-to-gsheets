8aa4bfe9f1ccc3ba313dd86d960d11888610eaf9
joao holz <joaopaulo.paivaholz@gmail.com>
2020-12-15 23:49:33 -0300

    comments

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from modules.from_csv_to_gs import send_csv
 from modules.from_db_to_csv import create_csv
 
 def main():
+    ## make query and write as csv in current directory
     create_csv()
+    ## read csv and write on a google sheets
     send_csv()
 
 if __name__ == "__main__":
